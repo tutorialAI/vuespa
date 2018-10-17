6a6cc59b5398d9a0be1f1aec65c509732fd4806a
tutorialAI <34983545+tutorialAI@users.noreply.github.com>
2018-10-17 20:16:58 +0300

    1

---
+++ src/assets.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="categories">
+    <li>lorem</li>
+    <li>lorem</li>
+    <li>lorem</li>
+    <li>lorem</li>
+    <li>lorem</li>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
